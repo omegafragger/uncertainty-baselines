db168bd4cda63a038afbe727530624ae507c85b8
Jishnu Mukhoti <omegafragger@gmail.com>
2022-10-03 11:48:31 +0100

    Testing torch

--- baselines/diabetic_retinopathy_detection/torch_test.py
+++ baselines/diabetic_retinopathy_detection/torch_test.py
@@ -1,122 +1,129 @@
+import datetime
 import os
+import pathlib
+import pprint
 import time
 
 from absl import app
 from absl import flags
 from absl import logging
-import numpy as np
 import tensorflow as tf
-import tensorflow_datasets as tfds
-import torch
-
-import torch_utils  # local file import
 import uncertainty_baselines as ub
 import utils  # local file import
-from tensorboard.plugins.hparams import api as hp
+import wandb
 
-# Hide any GPUs form TensorFlow. Otherwise TF might reserve memory and make
-# PyTorch crash with CUDA OoM error.
-tf.config.experimental.set_visible_devices([], 'GPU')
+from tensorboard.plugins.hparams import api as hp
 
 DEFAULT_NUM_EPOCHS = 90
 
 
-# Data load / output flags.
-
 class FLAGS:
-    output_dir = '/home/jishnu/Projects/db_retino_outputs'
-    data_dir = '/home/jishnu/Projects/ub_retinopathy'
+    output_dir = '/scratch-ssd/jisoti/ub_retinopathy_outputs'
+    eyepacs_data_dir = '/scratch-ssd/jisoti/ub_retinopathy/eyepacs'
+    aptos_data_dir = '/scratch-ssd/jisoti/ub_retinopathy/aptos'
     use_validation = True
-    base_learning_rate = 4e-4
-    one_minus_momentum = 0.1
-    lr_warmup_epochs = 20
+    use_test = True
+    preproc_builder_config = 'btgraham-300'
+    dr_decision_threshold = 'moderate'
+    load_from_checkpoint = False
+    checkpoint_dir = None
+    cache_eval_datasets = False
+    
+    use_wandb = False
+    wandb_dir = 'wandb'
+    project = 'ub-debug'
+    exp_name = None
+    exp_group = None
+    
+    distribution_shift = 'severity'
+    load_train_split = True
+    
+    base_learning_rate = 0.023072
+    final_decay_factor = 0.01
+    one_minus_momentum = 0.0098467
+    lr_schedule = 'step'
+    lr_warmup_epochs = 1
+    lr_decay_ratio = 0.2
+    lr_decay_epochs = ['30', '60']
+    
     seed = 42
-    l2 = 5e-5
+    class_reweight_mode = None
+    l2 = 0.00010674
     train_epochs = DEFAULT_NUM_EPOCHS
-    train_batch_size = 16
-    eval_batch_size = 32
+    per_core_batch_size = 32
     checkpoint_interval = 25
-    dropout_rate = 0.1
-    num_dropout_samples_eval = 10
     num_bins = 15
+    force_use_cpu = False
     use_gpu = True
+    use_bfloat16 = False
+    num_cores = 1
+    tpu = None
 
-tf.io.gfile.makedirs(FLAGS.output_dir)
-logging.info('Saving checkpoints at %s', FLAGS.output_dir)
 
-# Set seeds
 tf.random.set_seed(FLAGS.seed)
-np.random.seed(FLAGS.seed)
-torch.manual_seed(FLAGS.seed)
+wandb_run = None
+output_dir = FLAGS.output_dir
 
-# Resolve CUDA device(s)
-if FLAGS.use_gpu and torch.cuda.is_available():
-    print('Running model with CUDA.')
-    device = 'cuda:0'
+tf.io.gfile.makedirs(output_dir)
+
+# Log Run Hypers
+hypers_dict = {
+  'per_core_batch_size': FLAGS.per_core_batch_size,
+  'base_learning_rate': FLAGS.base_learning_rate,
+  'final_decay_factor': FLAGS.final_decay_factor,
+  'one_minus_momentum': FLAGS.one_minus_momentum,
+  'l2': FLAGS.l2
+}
+
+# Initialize distribution strategy on flag-specified accelerator
+strategy = utils.init_distribution_strategy(FLAGS.force_use_cpu,
+                                          FLAGS.use_gpu, FLAGS.tpu)
+use_tpu = not (FLAGS.force_use_cpu or FLAGS.use_gpu)
+per_core_batch_size = FLAGS.per_core_batch_size * FLAGS.num_cores
+
+
+class_reweight_mode = FLAGS.class_reweight_mode
+if class_reweight_mode == 'constant':
+    class_weights = utils.get_diabetic_retinopathy_class_balance_weights()
 else:
-    print('Running model on CPU.')
-    device = 'cpu'
-
-train_batch_size = FLAGS.train_batch_size
-eval_batch_size = FLAGS.eval_batch_size // FLAGS.num_dropout_samples_eval
+    class_weights = None
 
 
-train_batch_size = FLAGS.train_batch_size
-eval_batch_size = FLAGS.eval_batch_size // FLAGS.num_dropout_samples_eval
-
-# As per the Kaggle challenge, we have split sizes:
-# train: 35,126
-# validation: 10,906
-# test: 42,670
-ds_info = tfds.builder('diabetic_retinopathy_detection').info
-steps_per_epoch = ds_info.splits['train'].num_examples // train_batch_size
-steps_per_validation_eval = (
-  ds_info.splits['validation'].num_examples // eval_batch_size)
-steps_per_test_eval = ds_info.splits['test'].num_examples // eval_batch_size
-
-data_dir = FLAGS.data_dir
-
-dataset_train_builder = ub.datasets.get('ub_diabetic_retinopathy_detection', split='train', data_dir=data_dir)
-dataset_train = dataset_train_builder.load(batch_size=train_batch_size)
-
-dataset_validation_builder = ub.datasets.get(
-  'ub_diabetic_retinopathy_detection',
-  split='validation',
-  data_dir=data_dir,
-  is_training=not FLAGS.use_validation)
-validation_batch_size = (
-  eval_batch_size if FLAGS.use_validation else train_batch_size)
-dataset_validation = dataset_validation_builder.load(
-  batch_size=validation_batch_size)
-if not FLAGS.use_validation:
-# Note that this will not create any mixed batches of train and validation
-# images.
-    dataset_train = dataset_train.concatenate(dataset_validation)
-
-dataset_test_builder = ub.datasets.get(
-  'ub_diabetic_retinopathy_detection', split='test', data_dir=data_dir)
-dataset_test = dataset_test_builder.load(batch_size=eval_batch_size)
+datasets, steps = utils.load_dataset(
+  train_batch_size=per_core_batch_size,
+  eval_batch_size=per_core_batch_size,
+  flags=FLAGS,
+  strategy=strategy)
 
 
-train_iterator = iter(dataset_train)
-val_iterator = iter(dataset_validation)
-test_iterator = iter(dataset_test)
+dataset_iterators = {
+    'train': iter(datasets['train']),
+    'in_domain_validation': iter(datasets['in_domain_validation']),
+    'ood_validation': iter(datasets['ood_validation']),
+    'in_domain_test': iter(datasets['in_domain_test']),
+    'ood_test': iter(datasets['ood_test'])
+}
 
-# Check dataset stats
+import torch
 
 def get_torch_inputs(inputs, batch_size, image_h, image_w, device):
-    print ('Here')
     images = inputs['features']
     labels = inputs['labels']
-    print ('Here')
     images = torch.from_numpy(images._numpy()).view(batch_size, 3, image_h, image_w).to(device)
     labels = torch.from_numpy(labels._numpy()).to(device).float()
     return images, labels
 
 # Train
-for step in range(steps_per_epoch):
-    inputs = next(train_iterator)
-    images, labels = get_torch_inputs(inputs, train_batch_size, image_h, image_w, device)
+image_h = 512
+image_w = 512
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# iD Train
+for step in range(steps['train']):
+    inputs = next(dataset_iterators['train'])
+    images, labels = get_torch_inputs(inputs, per_core_batch_size, image_h, image_w, device)
+    images = images.to(device)
+    labels = labels.to(device)
     print (images.shape)
     print (labels.shape)
     print (images.min())
@@ -125,10 +132,10 @@ for step in range(steps_per_epoch):
     print (labels.unique())
     break
     
-# Validation
-for step in range(steps_per_epoch):
-    inputs = next(val_iterator)
-    images, labels = get_torch_inputs(inputs, eval_batch_size, image_h, image_w, device)
+# iD val
+for step in range(steps['in_domain_validation']):
+    inputs = next(dataset_iterators['in_domain_validation'])
+    images, labels = get_torch_inputs(inputs, per_core_batch_size, image_h, image_w, device)
     print (images.shape)
     print (labels.shape)
     print (images.min())
@@ -137,10 +144,22 @@ for step in range(steps_per_epoch):
     print (labels.unique())
     break
 
-# Test
-for step in range(steps_per_epoch):
-    inputs = next(test_iterator)
-    images, labels = get_torch_inputs(inputs, eval_batch_size, image_h, image_w, device)
+# OoD Val
+for step in range(steps['ood_validation']):
+    inputs = next(dataset_iterators['ood_validation'])
+    images, labels = get_torch_inputs(inputs, per_core_batch_size, image_h, image_w, device)
+    print (images.shape)
+    print (labels.shape)
+    print (images.min())
+    print (images.max())
+    print (images.unique())
+    print (labels.unique())
+    break
+    
+# iD test
+for step in range(steps['in_domain_test']):
+    inputs = next(dataset_iterators['in_domain_test'])
+    images, labels = get_torch_inputs(inputs, per_core_batch_size, image_h, image_w, device)
     print (images.shape)
     print (labels.shape)
     print (images.min())
@@ -149,3 +168,57 @@ for step in range(steps_per_epoch):
     print (labels.unique())
     break
 
+# OoD test
+for step in range(steps['ood_test']):
+    inputs = next(dataset_iterators['ood_test'])
+    images, labels = get_torch_inputs(inputs, per_core_batch_size, image_h, image_w, device)
+    print (images.shape)
+    print (labels.shape)
+    print (images.min())
+    print (images.max())
+    print (images.unique())
+    print (labels.unique())
+    break
+
+
+from tqdm import tqdm
+
+# iD Train
+for step in tqdm(range(steps['train'])):
+    inputs = next(dataset_iterators['train'])
+    images, labels = get_torch_inputs(inputs, per_core_batch_size, image_h, image_w, device)
+    images = images.to(device)
+    labels = labels.to(device)
+    pass
+    
+# iD val
+for step in tqdm(range(steps['in_domain_validation'])):
+    inputs = next(dataset_iterators['in_domain_validation'])
+    images, labels = get_torch_inputs(inputs, per_core_batch_size, image_h, image_w, device)
+    images = images.to(device)
+    labels = labels.to(device)
+    pass
+
+# OoD Val
+for step in tqdm(range(steps['ood_validation'])):
+    inputs = next(dataset_iterators['ood_validation'])
+    images, labels = get_torch_inputs(inputs, per_core_batch_size, image_h, image_w, device)
+    images = images.to(device)
+    labels = labels.to(device)
+    pass
+    
+# iD test
+for step in tqdm(range(steps['in_domain_test'])):
+    inputs = next(dataset_iterators['in_domain_test'])
+    images, labels = get_torch_inputs(inputs, per_core_batch_size, image_h, image_w, device)
+    images = images.to(device)
+    labels = labels.to(device)
+    pass
+
+# OoD test
+for step in tqdm(range(steps['ood_test'])):
+    inputs = next(dataset_iterators['ood_test'])
+    images, labels = get_torch_inputs(inputs, per_core_batch_size, image_h, image_w, device)
+    images = images.to(device)
+    labels = labels.to(device)
+    pass
